a2871bc04ff4790bdbc73b208920faa730c4454a
wanghai <1608776292@qq.com>
2025-03-02 01:11:12 +0800

    3.1

--- internal/main.go
+++ internal/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/lalalalade/webook/internal/web"
+	"strings"
+	"time"
 )
 
 func main() {
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowMethods:  []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:  []string{"Content-Type", "Authorization"},
+		ExposeHeaders: []string{"x-jwt-token"},
+		// 是否允许带 cookie 之类的东西
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "yourcompany.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
 	server.Run(":8080")
---
+++ internal/repository/dao/user.go
@@ -0,0 +1,8 @@
+package dao
+
+type User struct {
+	Addr Address
+}
+
+type Address struct {
+}
